1f106472422ec5e56913744957c55b68b9276483
yanhackcode15 <iamyanhu@gmail.com>
2024-03-31 15:02:06 -0700

    rider dashboard displaying accepted ride status

--- frontend/src/components/CurrentRideStatus.js
+++ frontend/src/components/CurrentRideStatus.js
@@ -6,22 +6,27 @@ function CurrentRideStatus({rideId}) {
   const [ride, setRide] = useState(null);
 
   useEffect(() => {
-    const fetchCurrentRide = async () => {
+    const fetchLatestRide = async () => {
       const token = localStorage.getItem('authToken');
-      
+      const latestRideId = localStorage.getItem('latestRideId')
+      if(!latestRideId || !token) {
+        console.log('No ride ID found or user is not authenticated');
+        return;
+      }
+
       try {
-        const response = await axios.get(`${process.env.REACT_APP_TRIP_SERVICE_URL}/rides/${rideId}`, {
+        const response = await axios.get(`${process.env.REACT_APP_TRIP_SERVICE_URL}/rides/${latestRideId}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
         setRide(response.data);
       } catch (error) {
-        console.error('Failed to fetch current ride:', error);
+        console.error('Failed to fetch current ride:', error.message);
       }
     };
 
-    fetchCurrentRide();
+    fetchLatestRide();
   }, []);
   if (!ride) {
     return <div>No current ride.</div>;
